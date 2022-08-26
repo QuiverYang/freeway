fddcdbb0fb900a05bb5b8baf6e51d058d3294b06
YangMongLin <m778677@yahoo.com.tw>
2022-08-26 11:00:13 +0800

    Add - camera Feature model

---
+++ free way/Camera Feature/SpeedCamera.swift
@@ -0,0 +1,26 @@
+//
+//  SpeedCamera.swift
+//  free way
+//
+//  Created by Menglin Yang on 2022/8/25.
+//
+
+import Foundation
+public enum Direction : String {
+    case north = "北向"
+    case south = "南向"
+    case east = "東向"
+    case west = "西向"
+    case northSouth = "南北向"
+    case esatWest = "東西向"
+    case empty = "無"
+}
+public struct SpeedCamera {
+    public var id : String
+    public var longtitude : Double
+    public var latitude : Double
+    public var memo : String
+    public var direction : Direction
+    public var road : String
+    public var roadMarker : Double
+}
